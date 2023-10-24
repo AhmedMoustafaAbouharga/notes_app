93f57281f6f1996cf915ede42282962e5ed30f70
my updates <AhmedMoustafaAbouharga>
2023-10-24 17:14:01 +0200

    create notes view body

---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/widgets/custom_app_bar.dart';
+
+import 'custom_note_item.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
+
+
