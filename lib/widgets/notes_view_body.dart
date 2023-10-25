e4550f9985809e81de4e77f289dbc31933399a86
my updates <AhmedMoustafaAbouharga>
2023-10-25 16:26:02 +0200

    create notes list view & add it

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/notes_list_view.dart';
 
 import 'custom_note_item.dart';
 
@@ -24,17 +25,4 @@ class NotesViewBody extends StatelessWidget {
   }
 }
 
-class NotesListView extends StatelessWidget {
-  const NotesListView({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding:  EdgeInsets.symmetric(vertical: 8.0),
-            child:  NoteItem(),
-          );
-        });
-  }
-}
