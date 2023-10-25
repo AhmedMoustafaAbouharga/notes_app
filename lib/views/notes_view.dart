b719a91a6062e9abe6ef0242b1dd62e43e913041
my updates <AhmedMoustafaAbouharga>
2023-10-25 17:14:04 +0200

    show modal bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -9,9 +10,14 @@ class NotesView extends StatelessWidget {
     return Scaffold(
       body: const NotesViewBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(context: context, builder:(context){
+            return const AddNoteBottomSheet();
+          } );
+        },
         child:const Icon(Icons.add),
       ),
     );
   }
 }
+
